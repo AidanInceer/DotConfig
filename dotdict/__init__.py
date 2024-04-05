f1d1268e0dd474911713c8273f79b950044ee6d7
AidanInceer <aidaninceer0@gmail.com>
2024-04-05 15:45:06 +0100

    fix: test coverage and simplify code

--- dotdict/__init__.py
+++ dotdict/__init__.py
@@ -1,47 +1,25 @@
 from __future__ import annotations
 
-from typing import Union
 
+class DotDict:
+    def __init__(self, d: dict):
 
-class DotDict(dict):
-    def __getattr__(self, attr: Union[DotDict, dict, list, str]):
-        """
-        Retrieve the value of the attribute from the configuration dictionary.
+        for key, value in d.items():
 
-        If the attribute exists in the configuration dictionary, it returns the value.
-        If the value is a dictionary, it returns a new instance of DotDict with
-        the value as the configuration dictionary.
-        If the value is a list, it returns a new list with each item converted to
-        DotDict if it is a dictionary, otherwise it returns the item as is.
-
-        Args:
-            attr (str): The name of the attribute to retrieve.
-
-        Returns:
-            The value of the attribute.
-
-        Raises:
-            AttributeError: If the attribute does not exist in the configuration dictionary.
-        """
-        if attr in self:
-            value = self[attr]
             if isinstance(value, dict):
-                return DotDict(value)
+                setattr(self, key, DotDict(value))
             elif isinstance(value, list):
-                return [
-                    (
-                        DotDict(item[0])
-                        if isinstance(item, list)
-                        and len(item) == 1
-                        and isinstance(item[0], dict)
-                        else (DotDict(item) if isinstance(item, dict) else item)
-                    )
-                    for item in value
-                ]
-            return value
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{attr}'"
-        )
+                setattr(
+                    self,
+                    key,
+                    [
+                        DotDict(item) if isinstance(item, dict) else item
+                        for item in value
+                    ],
+                )
+            else:
+                setattr(self, key, value)
 
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
+    def __repr__(self):
+        items = ", ".join([f"{key}: {value}" for key, value in self.__dict__.items()])
+        return f"{type(self).__name__}: {{{items}}}"
