5e0c7e968b417a328fa6c736e485629887a80a4c
AidanInceer <aidaninceer0@gmail.com>
2024-04-06 11:57:08 +0100

    chore: update configuration and pre commits

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="dotcon",
-    version="0.2.1",
-    description="A simple package which converts a standard python dictionary to a dot accessible object.",
-    author="Aidan Inceer",
-    packages=find_packages(),
-)
